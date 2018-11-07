d5cc6747e30f606227146c456e56a3c724c8a558
klgd <slice1213@gmail.com>
2018-11-07 20:43:20 +0800

    remove certbot

--- certbot/run-certbot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-certbot certonly --webroot -w /var/www/letsencrypt -d $CN --agree-tos -m $EMAIL --non-interactive --no-eff-email
-# certbot --help
-# cat /var/log/letsencrypt/letsencrypt.log
-# cp /etc/letsencrypt/archive/$CN/cert1.pem /var/certs/cert1.pem
-# cp /etc/letsencrypt/archive/$CN/privkey1.pem /var/certs/privkey1.pem
